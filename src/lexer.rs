41d1ec01dddf16aaa7ce27ea0156dccdae6ae51e
Fabian Drinck <fabian.drinck@rwth-aachen.de>
2026-02-15 07:54:10 +0100

    Use Peekable and CharIndices to simplify

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,3 +1,6 @@
+use std::iter::Peekable;
+use std::str::CharIndices;
+
 #[derive(Debug, PartialEq)]
 pub enum TokenKind {
     Error,
@@ -30,78 +33,64 @@ impl Token {
 }
 
 pub struct Lexer<'s> {
-    source: &'s [u8],
-    offset: usize,
+    source: &'s str,
+    chars: Peekable<CharIndices<'s>>,
     tokens: Vec<Token>,
 }
 
 impl<'s> Lexer<'s> {
-    pub fn new(source: &'s [u8]) -> Self {
+    pub fn new(source: &'s str) -> Self {
         Self {
             source,
-            offset: 0,
+            chars: source.char_indices().peekable(),
             tokens: Vec::new(),
         }
     }
 
-    fn offset(&self) -> usize {
-        self.offset
+    fn peek(&mut self) -> Option<char> {
+        self.chars.peek().map(|(_, ch)| *ch)
     }
 
-    fn current(&mut self) -> Option<u8> {
-        self.source.get(self.offset).copied()
+    fn next(&mut self) -> Option<(usize, char)> {
+        self.chars.next()
     }
 
-    fn peek(&mut self) -> Option<u8> {
-        self.source.get(self.offset + 1).copied()
-    }
-
-    fn advance(&mut self, offset: usize) {
-        self.offset += offset;
-    }
-
-    fn bump(&mut self) {
-        self.advance(1);
-    }
-
-    fn slice(&self, start: usize, end: usize) -> &[u8] {
+    fn slice(&self, start: usize, end: usize) -> &str {
         &self.source[start..end]
     }
 
     fn push(&mut self, kind: TokenKind, size: usize) {
         self.tokens.push(Token::new(kind, size as u32));
-        self.bump(); // TODO: elegant or ugly?
     }
 
     fn push_single(&mut self, kind: TokenKind) {
         self.push(kind, 1);
     }
 
-    fn identifier(&mut self) {
-        let start = self.offset();
+    fn identifier(&mut self, start: usize) {
         let mut end = start + 1;
 
         while let Some(ch) = self.peek()
             && ch.is_ascii_alphanumeric()
         {
             end += 1;
-            self.bump();
+            self.next();
         }
         let length = end - start;
 
         match self.slice(start, end) {
-            b"export" => self.push(KwExport, length),
-            b"fn" => self.push(KwFn, length),
-            b"import" => self.push(KwImport, length),
-            b"let" => self.push(KwLet, length),
-            b"mut" => self.push(KwMut, length),
+            "export" => self.push(KwExport, length),
+            "fn" => self.push(KwFn, length),
+            "import" => self.push(KwImport, length),
+            "let" => self.push(KwLet, length),
+            "mut" => self.push(KwMut, length),
             _ => self.push(TokIdentifier, length),
         }
     }
 
     fn eol(&mut self) {
-        if let Some(b'\n') = self.peek() {
-            self.bump();
+        if let Some('\n') = self.peek() {
+            self.next();
             self.push(TokNewline, 2);
         } else {
             self.push_single(TokNewline);
@@ -109,17 +98,17 @@ impl<'s> Lexer<'s> {
     }
 
     pub fn go(mut self) -> Vec<Token> {
-        while let Some(ch) = self.current() {
+        while let Some((offset, ch)) = self.next() {
             match ch {
-                b' ' | b'\t' => self.push_single(TokSpace),
-                b':' => self.push_single(TokColon),
-                b'(' => self.push_single(TokLeftParen),
-                b'{' => self.push_single(TokLeftBrace),
-                b'}' => self.push_single(TokRightBrace),
-                b')' => self.push_single(TokRightParen),
-                b'\n' => self.push_single(TokNewline),
-                b'\r' => self.eol(),
-                ch if ch.is_ascii_alphabetic() => self.identifier(),
+                ' ' | '\t' => self.push_single(TokSpace),
+                ':' => self.push_single(TokColon),
+                '(' => self.push_single(TokLeftParen),
+                '{' => self.push_single(TokLeftBrace),
+                '}' => self.push_single(TokRightBrace),
+                ')' => self.push_single(TokRightParen),
+                '\n' => self.push_single(TokNewline),
+                '\r' => self.eol(),
+                ch if ch.is_ascii_alphabetic() => self.identifier(offset),
                 _ => self.push_single(Error),
             }
         }
@@ -134,7 +123,7 @@ mod test {
     #[test]
     fn simple() {
         let source = ":(){}abc123 \t\\let mut fn\n\r\n\r";
-        let actual = Lexer::new(&source.as_bytes()).go();
+        let actual = Lexer::new(&source).go();
         let expected = [
             Token::new(TokColon, 1),
             Token::new(TokLeftParen, 1),
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,6 @@ mod lexer;
 
 fn main() {
     let source = " {  } blabla123";
-    let tokens = lexer::Lexer::new(source.as_bytes()).go();
+    let tokens = lexer::Lexer::new(source).go();
     println!("{:?}", tokens);
 }
